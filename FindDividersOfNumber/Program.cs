220c78f8307eb8d4b72b119d09b63bc06165d21d
Alexander <pchp250g8@tutamail.com>
2026-03-28 12:34:43 +0300

    Fixed Source Code

--- FindDividersOfNumber/Program.cs
+++ FindDividersOfNumber/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Input an integer positive number");
 UInt64.TryParse(Console.ReadLine(),out ulong ulNumber);
-Console.WriteLine("The number {0} has the folowing dividers:");
+Console.WriteLine("The number {0} has the folowing divisors:");
 for(ulong i = 1; i <= ulNumber; i++)
 {
     if (ulNumber % i == 0)
